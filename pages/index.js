48caa799dbe2ddaef959bf9b3c7e98a62fe3ecd4
Imam <imam@adds.local>
2021-12-07 21:17:46 +0700

    init app nextJs Basic

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,43 +13,24 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <h1 className={styles.title}>Welcome to My Blog</h1>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <div className={styles.card}>
+            <Link href="/posts/first-post">
+              <h3>First Post</h3>
+            </Link>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry...
             </p>
-          </a>
+          </div>
+          <div className={styles.card}>
+            <Link href="/posts/second-post">
+              <h3>Second Post</h3>
+            </Link>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+            </p>
+          </div>
         </div>
       </main>
 
